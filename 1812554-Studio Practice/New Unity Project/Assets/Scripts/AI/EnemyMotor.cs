fc72c36da22a7bb29fb648cd719457b9d4f0db66
gui <guilhermessecoortiz@gmail.com>
2021-10-15 18:08:35 +0100

    EnemyMovement

--- 1812554-Studio Practice/New Unity Project/Assets/Scripts/AI/EnemyMotor.cs
+++ 1812554-Studio Practice/New Unity Project/Assets/Scripts/AI/EnemyMotor.cs
@@ -6,15 +6,31 @@ public class EnemyMotor : MonoBehaviour
 {
 
     Transform _transform;
+    Animator anim;
     public Transform MovePoint;
     public float Speed;
     private void Start()
     {
         _transform = transform;
+        MovePoint.parent = null;
+        anim = MovePoint.GetComponent<Animator>();
     }
     private void Update()
     {
         _transform.position = Vector3.MoveTowards(_transform.position, MovePoint.position, Time.deltaTime * Speed);
+
+
+      if(Vector3.Distance(_transform.position,MovePoint.position) < 0.2f)
+        {
+            anim.SetBool("animate", true);
+        }
+        else
+        {
+            anim.SetBool("animate", false);
+        }
     }
+
+
+
 
 }
