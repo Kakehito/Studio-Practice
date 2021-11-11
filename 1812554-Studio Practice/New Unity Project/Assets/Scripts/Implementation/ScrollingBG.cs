7282efc2559de0401cfdedd2934da740182dc050
gui <guilhermessecoortiz@gmail.com>
2021-11-11 14:20:43 +0000

    Scrolling BG

--- 1812554-Studio Practice/New Unity Project/Assets/Scripts/Implementation/ScrollingBG.cs
+++ 1812554-Studio Practice/New Unity Project/Assets/Scripts/Implementation/ScrollingBG.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class ScrollingBG : MonoBehaviour
 {
-    public float Speed;
 
+    private RawImage _img;
+    public float _Y;
+    public float _X;
+
+    private void Start()
+    {
+        _img = GetComponent<RawImage>();
+    }
 
     private void Update()
     {
-        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + (Speed * Time.deltaTime));
+        _img.uvRect = new Rect(_img.uvRect.position + new Vector2(_X, _Y) * Time.deltaTime, _img.uvRect.size);
     }
 
 
